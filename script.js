15d3359054eb3e70593929bf60b392e006dbaee0
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-04-16 23:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-//your JS code here. If required.
-function secondHighest(arr) {
-    if (arr.length < 2 || new Set(arr).size === 1) {
-        return -Infinity;
-    }
-
-    let max1 = -Infinity;
-    let Max2 = -Infinity;
-
-    for (let n of arr) {
-        if (n > max1) {
-            Max2 = max1;
-            max1 = n;
-        } else if (n > Max2 && n !== max1) {
-            Max2 = n;
-        }
-    }
-
-    return Max2;
+		if (arr.length < 2) {
+		   return -Infinity;
+}
+let max1 = -Infinity;
+let max2 = -Infinity;
+for (let i = 0; i < arr.length; i++) {
+   if (arr[i] > max1) {
+      max2 = max1;
+      max1 = arr[i];
+   } else if (arr[i] > max2 && arr[i] < max1) {
+      max2 = arr[i];
+   }
+}
+if (max2 === -Infinity) {
+   return -Infinity;
+} else {
+   return max2;
 }
